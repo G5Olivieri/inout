e9926673bb9012018c5e8706ddb1482dd8a6de88
Bad <glayson@vizir.com.br>
2021-03-19 20:56:09 -0300

    add get expenses summary command

--- frontend/src/core/transactions/commands/get-expenses-summary-command.ts
+++ frontend/src/core/transactions/commands/get-expenses-summary-command.ts
@@ -1,72 +1,28 @@
+import { EventPublisher } from '@app/core/events/event-publisher'
+
 interface SummaryFilter {
   month?: number
   year: number
   day?: number
 }
 
-interface ExpensesSummaryResponse {
-  value: string
-}
-
-class HttpResponse {
-  public getData(): any {
-    return { value: '100' }
-  }
-}
-
-interface HttpRequestOptions {
-  queryParams: Record<string, string | number | undefined | null>
-}
-
-export class HttpClient {
-  public async get(
-    url: string,
-    options: HttpRequestOptions
-  ): Promise<HttpResponse> {
-    console.log(url, options)
-    return new HttpResponse()
-  }
-}
-
-interface EventPublisher {
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  publish(event: object): void
-}
-
-class DomainEvents implements EventPublisher {
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  private readonly listeners = new Map<Function, unknown>()
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  public publish(event: object): void {
-    this.listeners.get(event.constructor)
-  }
-}
-
-class Summary {
+export class TransactionSummary {
   public constructor(public readonly value: string) {}
 }
 
+export class ExpensesSummaryFetchedEvent {
+  public constructor(public readonly summary: TransactionSummary) {}
+}
+
 export class GetExpensesSummaryCommand {
-  public constructor(
-    private readonly eventPublisher: EventPublisher,
-    private readonly httpClient: HttpClient
-  ) {}
+  public constructor(private readonly eventPublisher: EventPublisher) {}
 
-  public async execute({ month, year, day }: SummaryFilter): Promise<void> {
-    const response = await this.httpClient.get('/expenses/summary', {
-      queryParams: {
-        month,
-        year,
-        day,
-      },
-    })
-    const expensesSummary = response.getData() as ExpensesSummaryResponse
-    this.eventPublisher.publish(
-      new ExpensesSummaryFetchedEvent(this.mapResponseToDomain(expensesSummary))
-    )
-  }
-
-  private mapResponseToDomain(response: ExpensesSummaryResponse): Summary {
-    return new Summary(response.value)
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public async execute(_filter: SummaryFilter): Promise<void> {
+    setTimeout(() => {
+      this.eventPublisher.publish(
+        new ExpensesSummaryFetchedEvent(new TransactionSummary('100'))
+      )
+    }, 500)
   }
 }
