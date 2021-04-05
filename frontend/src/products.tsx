1827110a08dbb5101b5ed76336be3e326367ceaf
Bad <glayson@vizir.com.br>
2021-04-05 19:19:40 -0300

    add request fake products

--- frontend/src/products.tsx
+++ frontend/src/products.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export const Products: React.FC = () => (
-  <ul>
-    <li>Produto 1</li>
-    <li>Produto 2</li>
-    <li>Produto 3</li>
-  </ul>
-)
+interface Product {
+  name: string
+}
+
+export const Products: React.FC = () => {
+  const [products, setProducts] = useState<Array<string>>([])
+
+  useEffect(() => {
+    fetch('https://api.mocki.io/v1/1f8203e9')
+      .then((res) => res.json() as Promise<Array<Product>>)
+      .then((data) => setProducts(data.map((p) => p.name)))
+  })
+  return (
+    <div>
+      <h1>Produtos</h1>
+      <ul>
+        {products.map((product, i) => (
+          <li key={i}>{product}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
