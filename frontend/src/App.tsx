9e15185e9340e6412137d12dcd75c7b41e3e7f01
vinioliveirar <vvini01@hotmail.com>
2021-04-29 21:13:59 -0300

    add routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import {  
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <div>
+            <h1>Home</h1>
+          </div>
+        </Route>
+        <Route exact path='/products'>
+          <div>
+            <h1>Produtos</h1>
+          </div>
+        </Route>
+        <Route exact path='/sales'>
+          <div>
+            <h1>Vendas</h1>
+          </div>
+        </Route>
+        <Route exact path='/purchases'>
+          <div>
+            <h1>Compras</h1>
+          </div>
+        </Route>
+
+      </Switch>
+
+    </Router> 
   );
 }
 
